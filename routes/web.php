90ce15ae90ba82ab5bbdf7c95723f66bad593bcf
netraular <netraular@gmail.com>
2024-10-04 19:54:58 +0000

    Crada vista de estrellas.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
